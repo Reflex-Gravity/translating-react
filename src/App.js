473c4ec2a1b15b493b9683e860761c1274ad42ef
Joel D Souza <joel.dsouza@kapturecrm.com>
2020-04-16 21:33:04 +0530

    Added more examples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Translate from "./utilities/translator";
+import "./styles.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -17,15 +18,30 @@ class App extends React.Component {
   render() {
     return (
       <div className="flex">
+        <h1>React Translator</h1>
+        <p>Try changing the language from the dropdown.</p>
         <div style={{ marginBottom: "50px" }}>
+          <label>Language: </label>
           <select value={this.state.lang} onChange={this.handleLangChange}>
             <option value="en">English</option>
             <option value="es">Spanish</option>
           </select>
         </div>
-        <div>
-          <Translate lang={this.state.lang}>Search</Translate>
-        </div>
+        <p style={{ borderBottom: 1 }}>
+          <b>Different examples of the translated text:</b>
+        </p>
+        <ol>
+          <li>
+            <label style={{ marginRight: 10 }}>Plain text:</label>
+            <Translate lang={this.state.lang}>Search</Translate>
+          </li>
+          <li>
+            <label>Inside an html tag:</label>
+            <h4>
+              <Translate lang={this.state.lang}>conversation</Translate>
+            </h4>
+          </li>
+        </ol>
       </div>
     );
   }
